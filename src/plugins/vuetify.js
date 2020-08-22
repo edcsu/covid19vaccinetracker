9855d287b38324bbe7ec18dd1144921ffe97ab2d
Keith Ssewanonda <keith@laboremus.no>
2020-08-22 16:33:40 +0300

    chore: add assets

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,29 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
+import GithubComponent from '../Icons/GithubComponent'
+import VirusComponent from '../Icons/VirusComponent'
+import TwitterComponent from '../Icons/TwitterComponent'
+import LinkedinComponent from '../Icons/LinkedinComponent'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+    values: {
+      github: {
+        component: GithubComponent
+      },
+      virus: {
+        component: VirusComponent
+      },
+      twitter: {
+        component: TwitterComponent
+      },
+      linkedin: {
+        component: LinkedinComponent
+      }
+    }
+  }
 })
