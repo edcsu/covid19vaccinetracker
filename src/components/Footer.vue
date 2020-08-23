58a3fbe31e9670d98604281bdb468096a2baff84
Keith Ssewanonda <keith@laboremus.no>
2020-08-23 12:55:19 +0300

    feat: add header and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-footer app class="primary">
+    <v-card-text
+      :class="[
+        'white--text',
+        'd-flex',
+        'justify-space-between',
+        {
+          'flex-column': $vuetify.breakpoint.xs
+        }
+      ]"
+    >
+      <span class>
+        <strong>#STAYHOME</strong>
+      </span>
+      <span class>
+        <strong>#WEARMASKINPUBLIC</strong>
+      </span>
+      <span>
+        Visit
+        <a
+          href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="white--text"
+        >
+          WHO
+        </a>
+        and
+        <a
+          href="https://coronavirus.jhu.edu/map.html"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="white--text"
+        >
+          John hopkins University
+        </a>
+        for more info.
+      </span>
+      <span>
+        Data from
+        <a
+          href="https://disease.sh/docs"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="white--text"
+        >
+          NOVELCoVID19 API
+        </a>
+      </span>
+    </v-card-text>
+  </v-footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-app-bar app clipped-left color="primary" dark>
+    <!-- <v-app-bar-nav-icon @click="$emit('hidesidemenu')"></v-app-bar-nav-icon> -->
+    <v-icon class="svg-icon" v-text="'$vuetify.icons.virus'"></v-icon>
+    <div class="ml-1 d-flex align-center">
+      COVID19 APP VACCINE TRACKER
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <!-- <v-tooltip bottom nudge-left="150">
+      <template v-slot:activator="{ on }">
+        <v-btn
+          href="https://github.com/NovelCOVID/awesome-novelcovid"
+          target="_blank"
+          icon
+          v-on="on"
+        >
+          <v-icon class="">mdi-seal</v-icon>
+        </v-btn>
+      </template>
+      <span>
+        Recognised on Awesome NovelCOVID projects
+        <v-icon class="">mdi-hand-pointing-up</v-icon>
+      </span>
+    </v-tooltip> -->
+
+    <v-tooltip bottom nudge-left="70">
+      <template v-slot:activator="{ on }">
+        <v-btn
+          href="https://www.linkedin.com/in/ssewannonda-keith-edwin-443303129"
+          target="_blank"
+          icon
+          v-on="on"
+        >
+          <v-icon class="svg-icon" v-text="'$vuetify.icons.linkedin'"></v-icon>
+        </v-btn>
+      </template>
+      <span>
+        Connect on LinkedIn
+        <v-icon class="">mdi-hand-pointing-up</v-icon>
+      </span>
+    </v-tooltip>
+
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          href="https://twitter.com/skeith696"
+          target="_blank"
+          icon
+          v-on="on"
+        >
+          <v-icon class="svg-icon" v-text="'$vuetify.icons.twitter'"></v-icon>
+        </v-btn>
+      </template>
+      <span>
+        Follow on twitter
+        <v-icon class="">mdi-hand-pointing-up</v-icon>
+      </span>
+    </v-tooltip>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  name: 'Header',
+
+  data: () => ({
+    //
+  })
+}
+</script>
