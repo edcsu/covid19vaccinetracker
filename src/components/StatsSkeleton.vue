19fa130a8ce9688c68c6743d2e2d17f72d95b5cd
Keith Ssewanonda <keith@laboremus.no>
2020-12-24 18:01:07 +0300

    style:update vaccine candidates skeleton

--- src/components/StatsSkeleton.vue
+++ src/components/StatsSkeleton.vue
@@ -4,62 +4,12 @@
       <v-col cols="12" sm="6" md="6" lg="3" xl="3">
         <v-skeleton-loader
           class="mx-auto"
-          min-width="300"
-          max-height="100"
-          type="image"
-        ></v-skeleton-loader>
-      </v-col>
-      <v-col cols="12" sm="6" md="6" lg="2" xl="2">
-        <v-skeleton-loader
-          class="mx-auto"
           min-width="200"
           max-height="100"
           type="image"
         ></v-skeleton-loader>
       </v-col>
-      <v-col cols="12" sm="6" md="4" lg="3" xl="3">
-        <v-skeleton-loader
-          class="mx-auto"
-          min-width="300"
-          max-height="100"
-          type="image"
-        ></v-skeleton-loader>
-      </v-col>
-      <v-col cols="12" sm="6" md="4" lg="2" xl="2">
-        <v-skeleton-loader
-          class="mx-auto"
-          min-width="200"
-          max-height="100"
-          type="image"
-        ></v-skeleton-loader>
-      </v-col>
-      <v-col cols="12" sm="12" md="4" lg="2" xl="2">
-        <v-skeleton-loader
-          class="mx-auto"
-          min-width="200"
-          max-height="100"
-          type="image"
-        ></v-skeleton-loader>
-      </v-col>
-    </v-row>
-    <v-row class="mt-n4">
-      <v-col cols="12" sm="6" md="4" lg="2" xl="2">
-        <v-skeleton-loader
-          class="mx-auto"
-          min-width="200"
-          max-height="100"
-          type="image"
-        ></v-skeleton-loader>
-      </v-col>
-      <v-col cols="12" sm="6" md="4" lg="2" xl="2">
-        <v-skeleton-loader
-          class="mx-auto"
-          min-width="200"
-          max-height="100"
-          type="image"
-        ></v-skeleton-loader>
-      </v-col>
-      <v-col cols="12" sm="6" md="4" lg="2" xl="2">
+      <v-col cols="12" sm="6" md="6" lg="3" xl="3">
         <v-skeleton-loader
           class="mx-auto"
           min-width="200"
@@ -70,12 +20,46 @@
       <v-col cols="12" sm="6" md="6" lg="3" xl="3">
         <v-skeleton-loader
           class="mx-auto"
-          min-width="300"
+          min-width="200"
           max-height="100"
           type="image"
         ></v-skeleton-loader>
       </v-col>
-      <v-col cols="12" sm="12" md="6" lg="3" xl="3">
+      <v-col cols="12" sm="6" md="6" lg="3" xl="3">
+        <v-skeleton-loader
+          class="mx-auto"
+          min-width="200"
+          max-height="100"
+          type="image"
+        ></v-skeleton-loader>
+      </v-col>
+    </v-row>
+    <v-row class="mt-n2">
+      <v-col cols="12" sm="6" md="6" lg="3" xl="3">
+        <v-skeleton-loader
+          class="mx-auto"
+          min-width="200"
+          max-height="100"
+          type="image"
+        ></v-skeleton-loader>
+      </v-col>
+      <v-col cols="12" sm="6" md="6" lg="3" xl="3">
+        <v-skeleton-loader
+          class="mx-auto"
+          min-width="200"
+          max-height="100"
+          type="image"
+        ></v-skeleton-loader>
+      </v-col>
+      <v-col cols="12" sm="6" md="6" lg="3" xl="3">
+        <v-skeleton-loader
+          class="mx-auto"
+          min-width="200"
+          max-height="100"
+          type="image"
+        ></v-skeleton-loader>
+      </v-col>
+      <v-col cols="12" sm="6" md="6" lg="3" xl="3">
         <v-skeleton-loader
           class="mx-auto"
           min-width="300"
